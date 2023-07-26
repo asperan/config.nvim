33b530993a145126b53cb9ae11016cf2a3ef758d
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-26 16:57:50 +0200

    feat: add keymap to open netrw tree (and fix style)

--- lua/custom/remap/netrw.lua
+++ lua/custom/remap/netrw.lua
@@ -1,2 +1,3 @@
 -- Add a shortcut to open netrw buffer
-vim.keymap.set('n', '<c-o>', ":Explore<cr>", { silent = true})
+vim.keymap.set('n', '<c-o>', "<cmd>Explore<cr>", { silent = true })
+vim.keymap.set('n', '<c-s-o>', "<cmd>Ntree<cr>", { silent = true })
