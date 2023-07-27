eb538c82357c5598a84d8ee20671df9c603015b8
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-27 12:08:44 +0200

    refactor: add different behavior for tree exploring based on buffer type

--- lua/custom/remap/netrw.lua
+++ lua/custom/remap/netrw.lua
@@ -1,3 +1,16 @@
 -- Add a shortcut to open netrw buffer
 vim.keymap.set('n', '<c-o>', "<cmd>Explore<cr>", { silent = true })
-vim.keymap.set('n', '<c-s-o>', "<cmd>Ntree<cr>", { silent = true })
+-- [[ NOT WORKING
+vim.keymap.set('n', '<c-s-o>', function ()
+    local buffer_name = vim.api.nvim_buf_get_name(0)
+    local is_terminal = string.find(buffer_name, "^term:")
+    if is_terminal then
+        local terminal_directory = string.match(buffer_name, '^term:%/(%g%g-)%d+:')
+        local expanded_dir = vim.fn.expand(string.sub(terminal_directory, 2, -2))
+        -- vim.fn.chdir(expanded_dir)
+        vim.api.nvim_exec2(":Ntree " .. expanded_dir, {})
+    else
+        vim.api.nvim_exec2(":Ntree", {})
+    end
+end, { silent = true })
+-- ]]
