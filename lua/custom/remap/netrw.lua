d591d63ed6d18922b03b35c59b6af37fcb0078d6
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-28 10:32:02 +0200

    fix: make c-s-o work in terminal

--- lua/custom/remap/netrw.lua
+++ lua/custom/remap/netrw.lua
@@ -1,16 +1,13 @@
+require ("utility")
 -- Add a shortcut to open netrw buffer
 vim.keymap.set('n', '<c-o>', "<cmd>Explore<cr>", { silent = true })
--- [[ NOT WORKING
 vim.keymap.set('n', '<c-s-o>', function ()
     local buffer_name = vim.api.nvim_buf_get_name(0)
-    local is_terminal = string.find(buffer_name, "^term:")
-    if is_terminal then
-        local terminal_directory = string.match(buffer_name, '^term:%/(%g%g-)%d+:')
-        local expanded_dir = vim.fn.expand(string.sub(terminal_directory, 2, -2))
-        -- vim.fn.chdir(expanded_dir)
-        vim.api.nvim_exec2(":Ntree " .. expanded_dir, {})
-    else
-        vim.api.nvim_exec2(":Ntree", {})
-    end
+    local expanded_dir = op_ternary(
+        string.find(buffer_name, "^term:"), -- Check if buffer is a terminal
+        string.sub(string.match(buffer_name, '^term:%/(%g%g-)%d+:'), 2, -2), -- retrieve base dir from terminal name
+        vim.fn.getcwd() -- Get current directory
+    )
+    vim.api.nvim_exec2(":Explore " .. expanded_dir, {})
 end, { silent = true })
--- ]]
+
