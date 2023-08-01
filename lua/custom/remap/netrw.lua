8a41610efb0551bba833c06011820b2358e6f353
Alex Speranza <alex.speranza@studio.unibo.it>
2023-08-01 11:01:36 +0200

    fix: change the c-s-o shortcut to lazily evaluate strings

--- lua/custom/remap/netrw.lua
+++ lua/custom/remap/netrw.lua
@@ -3,10 +3,10 @@ require ("utility")
 vim.keymap.set('n', '<c-o>', "<cmd>Explore<cr>", { silent = true })
 vim.keymap.set('n', '<c-s-o>', function ()
     local buffer_name = vim.api.nvim_buf_get_name(0)
-    local expanded_dir = op_ternary(
+    local expanded_dir = op_lazy_ternary(
         string.find(buffer_name, "^term:"), -- Check if buffer is a terminal
-        string.sub(string.match(buffer_name, '^term:%/(%g%g-)%d+:'), 2, -2), -- retrieve base dir from terminal name
-        vim.fn.getcwd() -- Get current directory
+        function() return string.sub(string.match(buffer_name, '^term:%/(%g%g-)%d+:'), 2, -2) end, -- retrieve base dir from terminal name
+        function() return vim.fn.getcwd() end -- Get current directory
     )
     vim.api.nvim_exec2(":Explore " .. expanded_dir, {})
 end, { silent = true })
