3be8c123c9d3b816fd44dd7a15e05d0c22801755
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-08 16:54:16 +0100

    revert: revert install of bashls

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -10,7 +10,6 @@ end
 --    Add any additional override configuration in the following tables. They will be passed to
 --    the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-    bashls = {},
     rust_analyzer = {},
     lua_ls = {
         Lua = {
