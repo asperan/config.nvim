42efb6a2b9aeced40a0a12358741106d81f6692a
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-08 16:13:58 +0100

    style: change colorscheme

--- lua/plugin/lazy.lua
+++ lua/plugin/lazy.lua
@@ -27,12 +27,12 @@ require("lazy").setup({
     },
     -- the colorscheme should be available when starting Neovim
     {
-        "folke/tokyonight.nvim",
+        "oxfist/night-owl.nvim",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-          -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight-moon]])
+            -- load the colorscheme here
+            vim.cmd.colorscheme("night-owl")
         end,
     },
     {
